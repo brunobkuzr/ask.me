1b0f6314d87163310d4daf267762488a278e8cb5
brunobkuzr <30652715+brunobkuzr@users.noreply.github.com>
2022-03-16 18:31:40 -0300

    exemplos

--- src/screen/demo.js
+++ src/screen/demo.js
@@ -3,8 +3,7 @@ import Button from '@mui/material/Button';
 import Astronaut from "../static/astronaut.jpg"
 
 
-import { useHistory } from "react-router-dom";
-
+import {useHistory} from "react-router-dom";
 
 
 import Pagination from '@mui/material/Pagination';
@@ -14,14 +13,14 @@ import axios from "axios";
 const Demo = () => {
     const [campo, setCampo] = useState('');
     const [page, setPage] = useState(1);
-
+    const [itens, setItens] = useState([])
     const navigate = useHistory();
     const buscaDados = async (e) => {
-        console.log('https://hackathon.lince.com.br/api/exemplo?paginaAtual=' + { page }  + '&itemsPorPagina=5')
+        //console.log('https://hackathon.lince.com.br/api/exemplo?paginaAtual=' + {page} + '&itemsPorPagina=5')
         try {
-         const { data } = await axios.get('https://hackathon.lince.com.br/api/exemplo?paginaAtual=' + {page} + '&itemsPorPagina=5');
-
-            console.log(data?.data)
+            const {data} = await axios.get('https://hackathon.lince.com.br/api/exemplo');
+            console.log(data?.itens)
+            setItens(data?.itens)
         } catch (error) {
             console.log('não deu bom')
             console.log(error)
@@ -35,12 +34,12 @@ const Demo = () => {
     }
 
 
-    const mudaPagina = (event, value) =>{
+    const mudaPagina = (event, value) => {
         setPage(value)
         buscaDados();
 
     }
-    const mudaTela = async (e) =>{
+    const mudaTela = async (e) => {
         e.preventDefault();
         navigate.push('/demo');
     }
@@ -51,7 +50,7 @@ const Demo = () => {
                 <ul>
                     <li>Deve ser importada, exemplo: <b>import Astronaut from "../static/astronaut.jpg"</b></li>
                 </ul>
-                <br />
+                <br/>
                 <img
                     alt=""
                     src={Astronaut}
@@ -63,25 +62,30 @@ const Demo = () => {
             <hr/>
             <section>
                 <h2>Botões e gerenciamento de rota</h2>
-                <br />
+                <br/>
                 <ul>
-                    <li>Button é um componente, precisa ser importado: <b>import Button from '@mui/material/Button';</b></li>
+                    <li>Button é um componente, precisa ser importado: <b>import Button from '@mui/material/Button';</b>
+                    </li>
                 </ul>
-                <Button variant="contained" color="success" onClick={ fazAcao }>
+                <Button variant="contained" color="success" onClick={fazAcao}>
                     Clique aqui para executar uma ação
                 </Button>
-                <p> { campo }</p>
-                <br />
-
-                <Button variant="contained" color="success" onClick={ mudaTela }>
+                <p> {campo}</p>
+                <br/>
+                <Button variant="contained" color="success" onClick={mudaTela}>
                     Clique aqui para mudar de página
                 </Button>
             </section>
-            <hr />
+            <hr/>
             <section>
                 <h2>Listas e paginação</h2>
+                <ul>
+                    {itens.map( item => (
+                        <li> { item }</li>
+                    ))}
+                </ul>
                 <Stack spacing={2}>
-                    <Pagination count={5} shape="rounded" page={ page } onChange={ mudaPagina } />
+                    <Pagination count={5} shape="rounded" page={page} onChange={mudaPagina}/>
                 </Stack>
             </section>
         </>
